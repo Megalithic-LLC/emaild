3e3d9f4d1bfd11379883196962be2e79dfa3ecff
David Rauschenbach <davidr@imap.cc>
2019-09-28 17:27:54 -0700

    Use new DAOs where applicable

--- imapendpoint/imapbackend/mailbox_set_subscribed.go
+++ imapendpoint/imapbackend/mailbox_set_subscribed.go
@@ -1,18 +1,11 @@
 package imapbackend
 
 import (
-	"github.com/asdine/genji"
 	"github.com/docktermj/go-logger/logger"
 )
 
 func (self *Mailbox) SetSubscribed(subscribed bool) error {
 	logger.Tracef("Mailbox:SetSubscribed(%v)", subscribed)
-	return self.backend.db.Update(func(tx *genji.Tx) error {
-		table, err := tx.GetTable("mailboxes")
-		if err != nil {
-			return err
-		}
-		self.model.Subscribed = subscribed
-		return table.Replace([]byte(self.model.ID), self.model)
-	})
+	self.model.Subscribed = subscribed
+	return self.backend.mailboxesDAO.Replace(self.model)
 }
--- imapendpoint/imapbackend/user_create_mailbox.go
+++ imapendpoint/imapbackend/user_create_mailbox.go
@@ -2,25 +2,15 @@ package imapbackend
 
 import (
 	"github.com/Megalithic-LLC/on-prem-emaild/model"
-	"github.com/asdine/genji"
 	"github.com/docktermj/go-logger/logger"
-	"github.com/rs/xid"
 )
 
 func (self *User) CreateMailbox(name string) error {
 	logger.Tracef("User:CreateMailbox(%s)", name)
-	return self.backend.db.Update(func(tx *genji.Tx) error {
-		table, err := tx.GetTable(model.MailboxTable)
-		if err != nil {
-			return err
-		}
-		mailbox := &model.Mailbox{
-			ID:          xid.New().String(),
-			AccountID:   self.account.ID,
-			Name:        name,
-			UidValidity: 1,
-		}
-		_, err = table.Insert(mailbox)
-		return err
-	})
+	mailbox := &model.Mailbox{
+		AccountID:   self.account.ID,
+		Name:        name,
+		UidValidity: 1,
+	}
+	return self.backend.mailboxesDAO.Create(mailbox)
 }
